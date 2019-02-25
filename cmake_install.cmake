c9f892fcbc498cc25b5a4858af40209257777c44
kdeyev <k.deyev@gmail.com>
2019-02-25 22:27:49 +0200

    Initial sources

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -33,37 +33,37 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1" TYPE FILE FILES
-    "D:/QJsonChannel/QJSonChannel1Config.cmake"
-    "D:/QJsonChannel/QJSonChannel1ConfigVersion.cmake"
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1" TYPE FILE FILES
+    "D:/QJsonChannel/QJsonChannel1Config.cmake"
+    "D:/QJsonChannel/QJsonChannel1ConfigVersion.cmake"
     )
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1/QJSonChannel1Targets.cmake")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1/QJsonChannel1Targets.cmake")
     file(DIFFERENT EXPORT_FILE_CHANGED FILES
-         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1/QJSonChannel1Targets.cmake"
-         "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJSonChannel1/QJSonChannel1Targets.cmake")
+         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1/QJsonChannel1Targets.cmake"
+         "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJsonChannel1/QJsonChannel1Targets.cmake")
     if(EXPORT_FILE_CHANGED)
-      file(GLOB OLD_CONFIG_FILES "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1/QJSonChannel1Targets-*.cmake")
+      file(GLOB OLD_CONFIG_FILES "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1/QJsonChannel1Targets-*.cmake")
       if(OLD_CONFIG_FILES)
-        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1/QJSonChannel1Targets.cmake\" will be replaced.  Removing files [${OLD_CONFIG_FILES}].")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1/QJsonChannel1Targets.cmake\" will be replaced.  Removing files [${OLD_CONFIG_FILES}].")
         file(REMOVE ${OLD_CONFIG_FILES})
       endif()
     endif()
   endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJSonChannel1/QJSonChannel1Targets.cmake")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJsonChannel1/QJsonChannel1Targets.cmake")
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJSonChannel1/QJSonChannel1Targets-debug.cmake")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJsonChannel1/QJsonChannel1Targets-debug.cmake")
   endif()
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJSonChannel1/QJSonChannel1Targets-minsizerel.cmake")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJsonChannel1/QJsonChannel1Targets-minsizerel.cmake")
   endif()
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJSonChannel1/QJSonChannel1Targets-relwithdebinfo.cmake")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJsonChannel1/QJsonChannel1Targets-relwithdebinfo.cmake")
   endif()
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJSonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJSonChannel1/QJSonChannel1Targets-release.cmake")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/QJsonChannel1" TYPE FILE FILES "D:/QJsonChannel/CMakeFiles/Export/lib/cmake/QJsonChannel1/QJsonChannel1Targets-release.cmake")
   endif()
 endif()
 
