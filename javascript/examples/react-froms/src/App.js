c427352a851950769725051aa444372ae80520c0
kdeyev <k.deyev@gmail.com>
2019-03-06 09:48:34 +0200

    react forms example initial submit

---
+++ javascript/examples/react-froms/src/App.js
@@ -0,0 +1,252 @@
+import React, { Component } from "react";
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import Form from "react-jsonschema-form";
+import { shouldRender } from "../src/utils";
+
+import "codemirror/mode/javascript/javascript";
+
+import { samples } from "./samples";
+
+// Import a few CodeMirror themes; these are used to match alternative
+// bootstrap ones.
+import "codemirror/lib/codemirror.css";
+
+
+const log = type => console.log.bind(console, type);
+const fromJson = json => JSON.parse(json);
+const toJson = val => JSON.stringify(val, null, 2);
+
+const cmOptions = {
+  height: "auto",
+  viewportMargin: Infinity,
+  mode: {
+    name: "javascript",
+    json: true,
+    statementIndent: 2,
+  },
+  lineNumbers: true,
+  lineWrapping: true,
+  indentWithTabs: false,
+  tabSize: 2
+};
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { valid: true, code: props.code };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({ valid: true, code: props.code });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) { 
+    return shouldRender(this, nextProps, nextState);
+  }
+
+  onCodeChange = (editor, metadata, code) => {
+    this.setState({ valid: true, code });
+    setImmediate(() => {
+      try {
+        this.props.onChange(fromJson(this.state.code));
+      } catch (err) {
+        this.setState({ valid: false, code });
+      }
+    });
+  };
+
+  render() {
+    const { title, readOnly} = this.props;
+    const icon = this.state.valid ? "ok" : "remove";
+    const cls = this.state.valid ? "valid" : "invalid";
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <span className={`${cls} glyphicon glyphicon-${icon}`} />
+          {" " + title}
+        </div>
+        <CodeMirror
+          value={this.state.code}
+          onChange={this.onCodeChange}
+          autoCursor={false}
+          options={Object.assign({}, {cmOptions, readOnly})}
+        />
+      </div>
+    );
+  }
+}
+
+class Selector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { current: "Simple"};
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return shouldRender(this, nextProps, nextState);
+  }
+
+  onLabelClick = label => {
+    return event => {
+      event.preventDefault();
+      this.setState({ current: label });
+      setImmediate(() => this.props.onSelected(samples[label]));
+    };
+  };
+
+  render() {
+    return (
+      <ul className="nav nav-pills">
+        {Object.keys(samples).map((label, i) => {
+          return (
+            <li
+              key={i}
+              role="presentation"
+              className={this.state.current === label ? "active" : ""}>
+              <a href="#" onClick={this.onLabelClick(label)}>
+                {label}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // initialize state with Simple data sample
+    const { schema, uiSchema, formData, validate } = samples.Simple;
+    this.state = {
+      form: false,
+      schema,
+      uiSchema,
+      formData,
+      validate,
+      liveSettings: {
+        validate: true,
+        disable: false,
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.load(samples.Simple);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return shouldRender(this, nextProps, nextState);
+  }
+
+  load = data => {
+    // Reset the ArrayFieldTemplate whenever you load new data
+    const { ArrayFieldTemplate, ObjectFieldTemplate } = data;
+    // uiSchema is missing on some examples. Provide a default to
+    // clear the field in all cases.
+    const { uiSchema = {} } = data;
+    // force resetting form component instance
+    this.setState({ form: false }, _ =>
+      this.setState({
+        ...data,
+        form: true,
+        ArrayFieldTemplate,
+        ObjectFieldTemplate,
+        uiSchema,
+      })
+    );
+  };
+
+  onSchemaEdited = schema => this.setState({ schema });
+
+  onUISchemaEdited = uiSchema => this.setState({ uiSchema});
+
+  onFormDataEdited = formData => this.setState({ formData });
+
+  onFormDataChange = ({ formData }) =>
+    this.setState({ formData });
+
+  render() {
+    const {
+      schema,
+      uiSchema,
+      formData,
+      liveSettings,
+      validate,
+      ArrayFieldTemplate,
+      ObjectFieldTemplate,
+      transformErrors,
+    } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <div className="page-header">
+          <div className="row">
+              <Selector onSelected={this.load} />
+          </div>
+        </div>
+        <div className="col-sm-7">
+          <Editor
+            title="JSONSchema"
+            code={toJson(schema)}
+            readOnly={true}
+            onChange={this.onSchemaEdited}
+          />
+          <div className="row">
+            <div className="col-sm-6">
+              <Editor
+                title="UISchema"
+                code={toJson(uiSchema)}
+                onChange={this.onUISchemaEdited}
+              />
+            </div>
+            <div className="col-sm-6">
+              <Editor
+                title="formData"
+                code={toJson(formData)}
+                onChange={this.onFormDataEdited}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-5">
+          {this.state.form && (
+            <Form
+              ArrayFieldTemplate={ArrayFieldTemplate}
+              ObjectFieldTemplate={ObjectFieldTemplate}
+              liveValidate={liveSettings.validate}
+              disabled={liveSettings.disable}
+              schema={schema}
+              uiSchema={uiSchema}
+              formData={formData}
+              onChange={this.onFormDataChange}
+              onSubmit={({ formData }, e) => {
+                console.log("submitted formData", formData);
+                console.log("submit event", e);
+              }}
+              validate={validate}
+              onBlur={(id, value) =>
+                console.log(`Touched ${id} with value ${value}`)
+              }
+              onFocus={(id, value) =>
+                console.log(`Focused ${id} with value ${value}`)
+              }
+              transformErrors={transformErrors}
+              onError={log("errors")}>
+              <div className="row">
+                <div className="col-sm-3">
+                  <button className="btn btn-primary" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </Form>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
